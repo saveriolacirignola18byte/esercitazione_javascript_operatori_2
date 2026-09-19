6649b9f760177efefe243cd5b9350497f2cf424f
saveriolacirignola18byte <saveriolacirignola18@gmail.com>
2026-09-19 16:48:07 +0200

    Esercitazione javascript operatori 2

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 let annoPresente = 2026;
 
-console.log(annoPresente);
-
 let annoNascita = 1990;
-
-console.log(annoNascita);
 
 let età = annoPresente - annoNascita;
 
-console.log(età);
-
 let anniRimanenti = 100 - età;
-
-console.log(anniRimanenti);
 
 console.log(`Hai ${età} anni e ti mancano ${anniRimanenti} anni per compierne 100.`);
